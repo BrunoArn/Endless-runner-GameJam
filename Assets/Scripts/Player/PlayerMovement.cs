b109a588d68283f8b7792ed619b4c89d504afb46
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2025-08-25 17:15:42 -0300

    add movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    private Vector2 direction = Vector2.up + Vector2.right;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        playerInput.Movement.ChangeDirection.performed += ChangeDirection;
+
+    }
+
+    void OnEnable()
+    {
+        playerInput.Enable();
+    }
+    void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    private void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * direction.normalized);
+
+    }
+    private void ChangeDirection(InputAction.CallbackContext context)
+    {
+        Debug.Log("entrei");
+        if (direction == Vector2.up + Vector2.right)
+        {
+            direction = Vector2.up + Vector2.left;
+        }
+        else
+        {
+            direction = Vector2.up + Vector2.right;
+        }
+    }
+}
