d0357ba059b3ce7ad09e70392aa333f907794985
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2025-08-27 17:14:20 -0300

    fix bug

--- Assets/Scripts/BiscuitAdd.cs
+++ Assets/Scripts/BiscuitAdd.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class BiscuitAdd : MonoBehaviour
 {
-    [SerializeField] Score scoreInfo;
+    public Score scoreInfo;
     [SerializeField] float scoreAmount;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (scoreInfo != null)
             scoreInfo.AddScore(scoreAmount);
-            
+
         Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -12,8 +12,9 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] float spawnDistance;
     [SerializeField] int yMax, yMin;
 
-    //[Header("Biscuit")]
-    //[SerializeField] GameObject biscuitPrefab;
+    [Header("Biscuit")]
+    [SerializeField] GameObject biscuitPrefab;
+    [SerializeField] Score score;
 
 
     private Tilemap myTileMap;
@@ -23,6 +24,12 @@ public class MapGenerator : MonoBehaviour
 
     private int lastGeneratedX;
 
+    private void Awake()
+    {
+        lastGeneratedYFloor = yMin;
+        lastGeneratedYRoof = yMax;
+    }
+
     void Start()
     {
         myTileMap = GetComponent<Tilemap>();
@@ -30,9 +37,6 @@ public class MapGenerator : MonoBehaviour
 
         bounds = myTileMap.cellBounds;
         lastGeneratedX = bounds.xMax;
-
-        lastGeneratedYFloor = yMin;
-        lastGeneratedYRoof = yMax;
     }
 
     private void Update()
@@ -42,53 +46,54 @@ public class MapGenerator : MonoBehaviour
 
     private void CreateMap()
     {
-        SpawnTiles();
-        //SpawnBiscuit();
-        lastGeneratedX++;
-    }
-
-    private void SpawnTiles()
-    {
         float playerX = player.position.x;
         int targetTileX = Mathf.FloorToInt(playerX + spawnDistance);
 
         while (lastGeneratedX <= targetTileX)
         {
             //floor
-            //generate base Floor
-            myTileMap.SetTile(new Vector3Int(lastGeneratedX, yMin), tile);
-            myTileMap.SetTile(new Vector3Int(lastGeneratedX, yMin - 1), tile); // generate below
+            GenerateTiles(lastGeneratedX, yMin, -1, yMin, -1, ref lastGeneratedYFloor);
+            //roof
+            GenerateTiles(lastGeneratedX, yMax, 1, 1, yMax, ref lastGeneratedYRoof);
 
-            //generate variation Y
-            int floorY = lastGeneratedYFloor + Random.Range(-1, 2); // -1, 0 or 1
-            floorY = Mathf.Clamp(floorY, yMin, -1); // avoids 0 and its minimun
-
-            for (int y = yMin; y < floorY; y++)
-            {
-                Vector3Int cellPosition = new(lastGeneratedX, y, 0);
-                myTileMap.SetTile(cellPosition, tile);
-            }
-            lastGeneratedYFloor = floorY;
-
-            //Roof
-            //generate base Roof
-            myTileMap.SetTile(new Vector3Int(lastGeneratedX, yMax), tile);
-            myTileMap.SetTile(new Vector3Int(lastGeneratedX, yMax + 1), tile); // generate above
-
-            int roofY = lastGeneratedYRoof + Random.Range(-1, 2); // -1, 0 or 1
-            roofY = Mathf.Clamp(roofY, 1, yMax); // avoids 0 and its minimun
-
-            for (int y = roofY; y < yMax; y++)
-            {
-                Vector3Int cellPosition = new(lastGeneratedX, y, 0);
-                myTileMap.SetTile(cellPosition, tile);
-            }
-            lastGeneratedYRoof = roofY;
+            GenerateBiscuit(lastGeneratedX, lastGeneratedYFloor + 1);
+            lastGeneratedX++;
         }
     }
 
-    private void SpawnBiscuit()
+    private void GenerateTiles(int x, int baseY, int extraBasePosition, int clampMin, int clampMax, ref int lastY)
     {
+        myTileMap.SetTile(new Vector3Int(x, baseY), tile);
+        myTileMap.SetTile(new Vector3Int(x, baseY + extraBasePosition), tile); // generate below
+
+        //generate variation Y
+        int newY = lastY + Random.Range(-1, 2); // -1, 0 or 1
+        newY = Mathf.Clamp(newY, clampMin, clampMax); // avoids 0 and its minimun
+
+        //generate fillers an last
+        int from = Mathf.Min(baseY, newY);
+        int to = Mathf.Max(baseY, newY);
+        for (int y = from; y <= to; y++)
+        {
+            Vector3Int cellPosition = new(x, y, 0);
+            myTileMap.SetTile(cellPosition, tile);
+        }
+        lastY = newY;
+    }
+
+    private void GenerateBiscuit(int x, int y)
+    {
+        int chanceToSpawn = Random.Range(0, 100);
+        if (chanceToSpawn >= 80)
+        {
+            Vector3 worldPos = myTileMap.CellToWorld(new Vector3Int(x, y, 0)) + new Vector3(0.5f, 0.5f, 0);
+            GameObject biscuit = Instantiate(biscuitPrefab, worldPos, Quaternion.identity);
+            
+            if (biscuit.TryGetComponent<BiscuitAdd>(out BiscuitAdd biscuitScoreReference))
+            {
+                biscuitScoreReference.scoreInfo = score;
+            }
+        }
 
     }
 }
